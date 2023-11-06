512afd293f5eb0e472907add9120efc9ddca0411
eiletan <nathanfhlai@gmail.com>
2023-11-06 12:29:22 -0800

    create card container component

---
+++ frontend/src/components/CardContainer.jsx
@@ -0,0 +1,10 @@
+import {React} from 'react';
+
+export default function CardContainer(props) {
+
+    return (
+        <div className="container-md card-container">
+
+        </div>
+    );
+}
--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import '../css/HomePage.css';
 export default function HomePage() {
     return (
         <div className="home-page-container">
-            
+            <p className="text home-title">Sample Title Here</p>
         </div>
     );
 }
