a8b7960ffe41f5af83cd71c2f04801cf7caebfb8
eiletan <nathanfhlai@gmail.com>
2023-11-07 14:45:44 -0800

    Add option to provide classname to CardContainer to allow greater flexibility with styling

--- frontend/src/components/CardContainer.jsx
+++ frontend/src/components/CardContainer.jsx
@@ -3,12 +3,13 @@ import "../css/CardContainer.css";
 
 // A bootstrap card that acts as a content container for the website
 // Pass in header as props.header - this prop is required
-// Pass in footer as props.footer - this prop is optional
 // Pass in content as props.content - this prop is required
+// Pass in footer as props.footer - this prop is optional
+// Pass in additional className as props.extraClass - this prop is optional
 export default function CardContainer(props) {
 
     return (
-        <div className="container-md card-container">
+        <div className={props.extraClass ? `container-md card-container ${props.extraClass}` : "container-md card-container"}>
             <div class="card">
                 <div className="card-header card-container-header">
                     <span className="text">
