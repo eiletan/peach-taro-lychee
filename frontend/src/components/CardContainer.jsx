f955fdf6ede7f24f0764f334ddc3717c0a899da4
eiletan <nathanfhlai@gmail.com>
2023-11-07 13:07:37 -0800

    update font and add footer to card container

--- frontend/src/components/CardContainer.jsx
+++ frontend/src/components/CardContainer.jsx
@@ -1,19 +1,27 @@
 import {React} from 'react';
+import "../css/CardContainer.css";
 
 // A bootstrap card that acts as a content container for the website
 // Pass in header as props.header - this prop is required
+// Pass in footer as props.footer - this prop is optional
 // Pass in content as props.content - this prop is required
 export default function CardContainer(props) {
 
     return (
         <div className="container-md card-container">
             <div class="card">
-                <div class="card-header">
+                <div className="card-header card-container-header text">
                     {props.header ? props.header : "No card header provided"}
                 </div>
-                <div class="card-body">
+                <div className="card-body card-container-body text">
                     {props.content ? props.content : "No card content provided"}
                 </div>
+                {props.footer ? 
+                <div className="card-footer card-container-footer text">
+                    {props.footer}
+                </div> 
+                : 
+                null}
             </div>
         </div>
     );
--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,16 @@
 import {React} from 'react';
 import '../css/HomePage.css';
+import CardContainer from './CardContainer';
+import sectionData from "../assets/sections.json";
 
 export default function HomePage() {
+    
     return (
         <div className="home-page-container">
             <p className="text home-title">Sample Title Here</p>
+            {sectionData.map((val) => {
+                return <CardContainer header={val["header"]} footer={val["footer"]}></CardContainer>
+            })}
         </div>
     );
 }
