d669b22ebdc6a0965516ddcaa903da01f31f834d
eiletan <nathanfhlai@gmail.com>
2023-11-24 17:09:54 -0800

    taro bubble tea is really good

--- peach-taro-lychee/app/page.tsx
+++ peach-taro-lychee/app/page.tsx
@@ -1,17 +1,22 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import sectionData from "../assets/sections.json";
+import sectionData from "../assets/sections.json"
+import CardContainer from "../components/CardContainer"
+
 
 import "../css/App.css";
 import "../css/HomePage.css";
 
-export default function Home() {
+export default async function Home() {
+
+  const sections: any = sectionData;
+
   return (
     <div className="home-page-container">
-        <p className="text home-title">Sample Title Here</p>
-        {/* {sectionData.map((val) => {
+        <p className="text home-title">Peach Taro Lychee</p>
+        {sections.map((val: {header: string, footer: string}) => {
             return <CardContainer header={val["header"]} footer={val["footer"]}></CardContainer>
-        })} */}
+        })}
     </div>
 );
 }
