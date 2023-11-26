a5d7228fa83cf3021702e10479eec98bcc3b9541
eiletan <nathanfhlai@gmail.com>
2023-11-25 22:18:40 -0800

    move homepage to another route

---
+++ peach-taro-lychee/app/HomePage.tsx
@@ -0,0 +1,44 @@
+'use client';
+import React from 'react';
+import {useState} from 'react';
+import sectionData from "../assets/sections.json"
+import CardContainer from "../components/CardContainer"
+import List from '../components/List'
+import Button from '../components/Button';
+
+
+
+import "../css/App.css";
+import "../css/HomePage.css";
+
+export default function HomePage() {
+
+const [isEdit,setIsEdit] = useState<boolean>(false);
+  const sections: any = sectionData;
+
+  const list: Element | JSX.Element = <List isEdit={isEdit} list={[]}></List>;
+
+  const flexx: any = <div className="grid grid-cols-1 grid-container">{list}</div>
+
+  const cardTest: Element | JSX.Element = <CardContainer header="Furina" content={flexx} footer="Last Updated" extraClass="card-content" isEdit={isEdit}></CardContainer>;
+
+  const cardTest2: Element | JSX.Element = <CardContainer header="Focalors" content={flexx} footer="Last Updated" extraClass="card-content" isEdit={isEdit}></CardContainer>;
+
+  const cardTest3: Element | JSX.Element = <CardContainer header="Oratrice Mecanique D'Analyse Cardinale" footer="Last Updated" content={flexx} extraClass="card-content" isEdit={isEdit}></CardContainer>;
+
+  const flexContainer: any = <div className="grid grid-cols-1 md:grid-cols-2">
+    <div className="flex flex-col flex-auto">{<div className="flex-auto">{cardTest}</div>}</div>
+    <div className="flex flex-col flex-auto">{<div className="flex-auto">{cardTest2}</div>}</div>
+    <div className="flex flex-col flex-auto">{<div className="flex-auto">{cardTest3}</div>}</div>
+    </div>
+
+  return (
+    <div className="home-page-container">
+        <Button text={isEdit ? "Stop Editing" : "Edit"} bgcolor="bg-blue-700" color="text-slate-50" onClick={() => setIsEdit(prevIsEdit => !prevIsEdit)}></Button>
+        <p className="text home-title">Peach Taro Lychee</p>
+        {sections.map((val: {header: string, footer: string}) => {
+            return <CardContainer header={val["header"]} footer={val["footer"]} content={flexContainer} isEdit={isEdit}></CardContainer>
+        })}
+    </div>
+    );
+}
