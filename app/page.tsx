ea7088947c576e476fbac947bb622c5ceb902c63
eiletan <nathanfhlai@gmail.com>
2023-12-21 15:12:25 -0800

    Update schema and implement getCards function

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import HomePage from './HomePage'
+import prisma from '@/lib/prisma';
 
-export default function Home() {
- 
+
+async function getCards() {
+  const cards = await prisma.card.findMany({
+    select: {
+      id: true,
+      header: true,
+      updatedAt: true
+    }
+  });
+  console.log(cards);
+  return cards;
+}
+
+export default async function Home() {
+  const cards = await getCards();
+  console.log({cards});
   return (
     <HomePage></HomePage>
 );
