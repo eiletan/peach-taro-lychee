a25326fc9b366659b7c4fda52cd774ec66917e85
eiletan <nathanfhlai@gmail.com>
2024-01-19 11:50:46 -0800

    Fix caching issue in production

--- app/api/get-cards/route.ts
+++ app/api/get-cards/route.ts
@@ -2,6 +2,8 @@ import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server";
 
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const cards = await getCards();
     return NextResponse.json({cards});
